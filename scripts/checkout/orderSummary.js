d020fec4f2836a9ac596af6f4d52805315e93d04
Chirag Goyal <chiraggoyal4639@gmail.com>
2025-06-18 19:28:48 +0530

    use inheritance and polymorphism to add size chart

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -58,7 +58,7 @@ export function renderOrderSummary() {
               ${matchingProduct.name}
             </div>
             <div class="product-price js-product-price-${matchingProduct.id}">
-              $${matchingProduct.getPrice()}
+              ${matchingProduct.getPrice()}
             </div>
             <div class="product-quantity js-product-quantity-${productId}">
               <span>
