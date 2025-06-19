0d2cef8cd970fa4ed26a31f369ad3ad84caca6da
Chirag Goyal <chiraggoyal4639@gmail.com>
2025-06-19 19:50:33 +0530

    convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,11 +4,22 @@ import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts, loadProductsFetch} from '../data/products.js';
 // import '../data/cart-class.js';
 
+async function loadPage() {
+  await loadProductsFetch();
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+
+loadPage();
+
+/*
 loadProductsFetch().then(() => {
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
 })
+*/
 /*
 loadProducts (() => {
   renderCheckoutHeader();
